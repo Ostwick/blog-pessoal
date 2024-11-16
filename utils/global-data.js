68a1f217267c8cf5bca22df88fabcbf3f9f4741a
Henrique <48026099+Ostwick@users.noreply.github.com>
2024-11-16 07:39:36 -0300

    Melhorias
    Função getEnvVar:
    
        Criamos uma função utilitária chamada getEnvVar para buscar variáveis de ambiente. Ela verifica se a variável existe e, se não, utiliza o valor padrão. A função também faz o decodeURI automaticamente, o que facilita o código e evita repetições.
    
    Redução de Código:
    
        A função getEnvVar é reutilizada para buscar os valores de BLOG_NAME, BLOG_TITLE e BLOG_FOOTER_TEXT, evitando a repetição do mesmo padrão de verificação e tornando o código mais limpo e modular.
    
    Comentários:
    
        Adicionamos um comentário explicando o que a função getGlobalData faz, o que facilita o entendimento do código para outros desenvolvedores

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,14 +1,15 @@
-export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'DIO Blog';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'Blog em NextJS';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+// Função para obter as variáveis de ambiente com valor padrão, caso estejam ausentes.
+const getEnvVar = (key, defaultValue) => {
+  const value = process.env[key];
+  return value ? decodeURI(value) : defaultValue;
+};
 
+export const getGlobalData = () => {
+  const name = getEnvVar('BLOG_NAME', 'Ostwick Blog');
+  const blogTitle = getEnvVar('BLOG_TITLE', 'Blog em NextJS');
+  const footerText = getEnvVar('BLOG_FOOTER_TEXT', 'All rights reserved.');
+
+  // Retorna o objeto com os dados globais do site.
   return {
     name,
     blogTitle,
